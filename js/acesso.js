995453d0697b878f1e6c788df3b8736a5aec7ad2
Michelle Marques <michellea.m.silva@gmail.com>
2021-04-04 20:05:51 -0300

    modificaçoes em acesso

--- js/acesso.js
+++ js/acesso.js
@@ -1,10 +1,38 @@
-let botaoValidar = $('.botaoValidar').click(function (event) {
+let botaoValidar = document.querySelector('.botaoValidar');
+botaoValidar.addEventListener("click", function (event) {
     //  <-- barra comportamento padrão navegado -->
     event.preventDefault();
-    console.log("Fui clicado");
 
-   
+    let form = document.querySelector("#acesso");
+    let usuario = form.login.value;
+    let senha = form.senha.value;
+
+    if (usuario == 123 && senha == 123) {
+        ocultaAdicionaCardapio();
+        
+    } else {
+        //alert("senha e usuarios inválidos");
+    }
 });
+
+function ocultaAdicionaCardapio() {
+    let cardapios = document.querySelector(".cardapios");
+    cardapios.classList.add("oculta-cardapio");
+};
+
+
+
+
+function criaListaVegana() {
+    let botaoAdicionar = document.querySelector(".comida-vegana");
+    botaoAdicionar.addEventListener("click", function () {
+        let lista = document.querySelector(".cardapio-normal");
+        console.log(lista);
+    })
+
+
+
+}
 
 
 
