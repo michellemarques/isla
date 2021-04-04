e3459a8bde146dd1e09f65f50327bc751f8aee16
Michelle Marques <michellea.m.silva@gmail.com>
2021-04-04 16:49:39 -0300

    primeiro isla

---
+++ js/acesso.js
@@ -0,0 +1,10 @@
+let botaoValidar = $('.botaoValidar').click(function (event) {
+    //  <-- barra comportamento padrão navegado -->
+    event.preventDefault();
+    console.log("Fui clicado");
+
+   
+});
+
+
+
